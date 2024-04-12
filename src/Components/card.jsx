2d79a90f38b37b3e00957ccfa85efae919f64790
Kururpira <kururpira@inbox.ru>
2024-04-12 14:45:29 +0300

    commit

---
+++ src/Components/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Card(props) {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-title">{props.name}</h4>
+                <p className="card-text">{props.universe}</p>
+                <img src={props.url} alt={props.title}/>
+                <p className="card-text">{props.info}</p>
+            </div>
+            <div className="card-footer">
+            </div>
+        </div>
+    );
+}
+
+export default Card;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import HerousCard from './Components/herous_card.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <HerousCard/>
+  </React.StrictMode>,
+)
