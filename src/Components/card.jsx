778b174b042b1e93f3b3363412b2a9b64483476c
Kururpira <kururpira@inbox.ru>
2024-04-16 12:39:27 +0300

    commit

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+
 
 function Card(props) {
     return (
+       
         <div className="card">
             <div className="card-body">
                 <h4 className="card-title">{props.name}</h4>
                 <p className="card-text">{props.universe}</p>
-                <img src={props.url} alt={props.title}/>
+                <img className="card-img" src={props.url} alt={props.title}/>
                 <p className="card-text">{props.info}</p>
             </div>
+            
             <div className="card-footer">
             </div>
-        </div>
+       </div>
     );
 }
 
